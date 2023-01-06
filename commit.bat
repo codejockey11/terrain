4e115bfcad54fc37fd3b877a001358dcbd31f52a
codejockey11 <junk_1105@hotmail.com>
2023-01-06 18:52:19 -0500

    Initial commit

---
+++ commit.bat
@@ -0,0 +1,7 @@
+ECHO ON
+git checkout --orphan TEMP_BRANCH
+git add -A
+git commit -am "Initial commit"
+git branch -D main
+git branch -m main
+git push -f origin main
---
+++ terrain/terrain.bat
@@ -0,0 +1,14 @@
+REM bmp = new CBmpImage(argv[1]);
+REM	primSize = (INT)atoi(argv[2]);
+REM	vheight = (INT)atoi(argv[3]);
+REM	vertCount = (INT)atoi(argv[4]);
+REM	useUV = (INT)atoi(argv[5]);
+REM	average = (INT)atoi(argv[6]);
+REM	vertices = (INT)atoi(argv[7]);
+REM
+terrain.exe flat64x64.bmp 8 1 64 1 1 1
+REM terrain.exe heightmap64x64.bmp 32 1 32 1 1 1
+REM terrain.exe heightmap264x64.bmp 32 1 64 1 1 1
+REM terrain.exe heightmap512x512.bmp 8 1 8 1 1 1
+REM terrain.exe oceancliffs.bmp 32 4 256 1 1 1
+REM terrain.exe heightmap8x8.bmp 256 2 8 1 1 1
